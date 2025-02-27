d78b5b03275d3c713a9af6c2ff03939fa8ebeb30
ZainManzoor2003 <101002312+ZainManzoor2003@users.noreply.github.com>
2025-02-27 18:50:54 +0500

    Update index.js

--- Enrollment App/server/index.js
+++ Enrollment App/server/index.js
@@ -5,9 +5,15 @@ const cors = require('cors');
 const connection = require('./db')
 const router = require('./routers/router')
 const app = express();
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://fyp-enrollment-client.vercel.app'], // Replace with your allowed origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedhallowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Allowed methods
+};
 app.use(express.json())
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/', router);
 const port = 3001;
 
